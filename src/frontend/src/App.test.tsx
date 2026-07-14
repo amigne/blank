303505ad33f43a6038b0d3d586e3046ff341db8c
Yann GAUTERON <yann@gauteron.me>
2026-07-14 16:37:05 +0000

    Frontend

---
+++ src/frontend/src/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("renders the application", () => {
+    render(<App />);
+
+    expect(screen.getByRole("heading", { name: /get started/i })).toBeInTheDocument();
+  });
+});
